e1e55652eedad5ab5727392bfd1a648a3fc147af
eve00 <64850924+eve00@users.noreply.github.com>
2024-01-06 19:02:57 +0900

    detektの指摘を修正する

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     kotlin("jvm") version "1.9.0"
 
-    id("io.gitlab.arturbosch.detekt") version("1.23.4")
+    id("io.gitlab.arturbosch.detekt") version ("1.23.4")
     id("org.jlleitschuh.gradle.ktlint") version ("11.6.1")
 
     application
@@ -23,6 +25,30 @@ dependencies {
 
     testImplementation("org.http4k:http4k-client-jetty:4.48.0.0")
 }
+
+/*
+detekt {
+    toolVersion = "1.23.4"
+    config.setFrom(file("config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+    source.setFrom(".")
+
+    autoCorrect = true
+    parallel = true
+
+
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+        txt.required.set(false)
+        sarif.required.set(true)
+        md.required.set(true)
+    }
+}
+*/
 
 tasks.test {
     useJUnitPlatform()
